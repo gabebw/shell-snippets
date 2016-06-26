3606325540a90e4067b01bc743263b8b8289e596
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-26 16:09:24 -0700

    idempotently_join_heroku_app

---
+++ idempotently_join_heroku_app.sh
@@ -0,0 +1,9 @@
+# `heroku join` exits 1 if the user has already joined the app _and_ if the user
+# can't join the app. So, call `heroku join` but grep for a specific phrase to
+# see if a user successfully joined the app.
+if heroku join --app "$1" 2>&1 | grep -Fq 'already a collaborator'; then
+  printf 'You are a collaborator on the "%s" Heroku app\n' "$1"
+else
+  printf 'Ask for access to the "%s" Heroku app\n' "$1"
+  exit 1
+fi
